6f74cf53f8d003fb7ef1355011f99a2a35b654e1
kallial <aleksi@pullakahvit.fi>
2025-08-24 20:15:56 +0300

    Add NavBar layout and logo with resposive container

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,21 @@
-const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+import { useRef } from "react";
 
-export default NavBar
+const NavBar = () => {
+  const navContainerRef = useRef(null);
+
+  return (
+    <div ref={navContainerRef} className="fixed inset-x-0 z-50 h-16 border-none transition-all duration-700">
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="/img/logo-black.svg" alt="logo" className="w-10" />
+          </div>
+          <div className="lg:hidden">
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
